ab6c58c0473d5fb27a7377f23a45efa99e118d6a
Rob Merrill <robgmerrill@gmail.com>
2016-05-06 12:21:28 -0700

    added angulur directives and controller

--- shaping_up_with_angular/app.js
+++ shaping_up_with_angular/app.js
@@ -1,6 +1,26 @@
 (function() {
   var app = angular.module('dogStore', []);
-  app.controller('StoreController', function() {
 
+  app.controller('StoreController', function(){
+    this.products = dogs;
   });
+
+  var dogs = [
+    { 
+      name: 'Oso', 
+      breed: 'Golden Retriver', 
+      images: "img/golden-retriever.jpg" 
+    },
+    { 
+      name: 'Bernie', 
+      breed: 'Bernese Mountain Dog', 
+      images: "img/bernese.jpg"  
+    },
+    { 
+      name: 'Paws', 
+      breed: 'Nova Scotia Duck Toll Retriver', 
+      images: "img/nova-scotia.jpg"  
+    }
+  ];
 })();
+
